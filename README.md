053f0ab953f48acb7d4e2613e14678fecaf0efdf
uyinn@live.com <uyinn@live.com>
2017-11-16 10:46:15 +0800

    add init.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ubuntu-configure
+
+# 安装
+
+执行脚本即可
+
+```bash
+bash init.sh
+
+```
---
+++ init.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+#
+
+
+function ssh(){
+    [ -f ~/.ssh/config ] && {
+        mv ~/.ssh/config{,.bak_$(date +%F_%H)}
+    }
+    cp .ssh ~/
+}
+
+function pip(){
+    which pip || {
+        sudo apt install python-pip
+    }
+
+    cp .pip ~/
+}
+
+function stormssh(){
+    which storm || {
+        sudo pip install stormssh
+    }
+
+    [ -f ~/.stormssh/config ] && {
+        mv ~/.stormssh/config{,.bak_$(date +%F_%H)}
+    }
+    cp .stormssh ~/
+}
+
+function ohmyzsh(){
+    sudo apt install git zsh
+    sudo bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+
+
+select var in ssh pip stormssh ohmyzsh
+do
+    case $var in
+    all)
+        ssh; pip; stormssh; ohmyzsh ;;
+    ssh|pip|stormssh|ohmyzsh) 
+        $var ;;
+    *)
+        exit 1 ;;
+    esac
+
+done
